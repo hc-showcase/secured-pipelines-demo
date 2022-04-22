bcb946864d2bd93241e2e6d6fe02b23182be5d9f
Marc-Steffen Kaesz <mskaesz@protonmail.com>
2022-04-22 11:49:04 +0200

    aws and azure creds

--- scripts/04a_configure_aws_secrets.sh
+++ scripts/04a_configure_aws_secrets.sh
@@ -2,6 +2,24 @@
 
 . 99_source_temp_data.sh
 
-cd 04a_configure_aws_secrets
-terraform init
-terraform apply
+tf() {
+	cd 04a_configure_aws_secrets
+	terraform init
+	terraform apply --auto-approve
+}
+
+cmd() {
+	echo
+}
+
+case $1 in
+tf)
+	tf
+	;;
+cmd)
+	cmd
+	;;
+*)
+	echo "cmd for command line, tf for terraform"
+	;;
+esac
--- scripts/04b_configure_azure_secrets.sh
+++ scripts/04b_configure_azure_secrets.sh
@@ -1,20 +1,25 @@
 #!/bin/sh
 
-set -e
+. 99_source_temp_data.sh
 
-export VAULT_ADDR=http://127.0.0.1:8200
-export VAULT_TOKEN=root
-vault secrets disable azure
-vault secrets enable azure
-source  azure_creds
-echo $ARM_CLIENT_ID
-vault write azure/config \
-subscription_id=$ARM_SUBSCRIPTION_ID \
-tenant_id=$ARM_TENANT_ID \
-client_id=$ARM_CLIENT_ID \
-client_secret=$ARM_CLIENT_SECRET
+tf() {
+	cd 04b_configure_azure_secrets
+	terraform init
+	terraform apply --auto-approve
+}
 
-vault write azure/roles/myproject ttl=1h azure_roles=-<<EOF
+cmd() {
+	vault secrets disable azure
+	vault secrets enable azure
+	source azure_creds
+	echo $ARM_CLIENT_ID
+	vault write azure/config \
+		subscription_id=$ARM_SUBSCRIPTION_ID \
+		tenant_id=$ARM_TENANT_ID \
+		client_id=$ARM_CLIENT_ID \
+		client_secret=$ARM_CLIENT_SECRET
+
+	vault write azure/roles/myproject ttl=1h azure_roles=- <<EOF
     [
         {
             "role_name": "Contributor",
@@ -27,9 +32,9 @@ vault write azure/roles/myproject ttl=1h azure_roles=-<<EOF
     ]
 EOF
 
-vault read azure/creds/myproject
+	vault read azure/creds/myproject
 
-vault write azure/roles/myproject2 ttl=1h azure_roles=-<<EOF
+	vault write azure/roles/myproject2 ttl=1h azure_roles=- <<EOF
     [
         {
             "role_name": "Contributor",
@@ -42,4 +47,17 @@ vault write azure/roles/myproject2 ttl=1h azure_roles=-<<EOF
     ]
 EOF
 
-vault read azure/creds/myproject2
+	vault read azure/creds/myproject2
+}
+
+case $1 in
+tf)
+	tf
+	;;
+cmd)
+	cmd
+	;;
+*)
+	echo "cmd for command line, tf for terraform"
+	;;
+esac
