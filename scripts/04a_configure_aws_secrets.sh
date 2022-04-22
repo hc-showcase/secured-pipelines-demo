78a13f8960ed1939dc5c25e2cb02ca00d4eb9115
Marc-Steffen Kaesz <mskaesz@protonmail.com>
2022-04-22 12:34:15 +0200

    aws cmd

--- scripts/04a_configure_aws_secrets.sh
+++ scripts/04a_configure_aws_secrets.sh
@@ -9,7 +9,12 @@ tf() {
 }
 
 cmd() {
-	echo
+	vault secrets enable aws
+	vault write aws/config/root \
+		access_key=$AWS_ACCESS_KEY_ID
+	secret_key=$AWS_SECRET_ACCESS_KEY \
+		region=eu-central-1
+
 }
 
 case $1 in
