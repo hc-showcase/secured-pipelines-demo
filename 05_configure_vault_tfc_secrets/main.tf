d8b2f4e6f1be84c895999ed0ec85d66071b0089d
Marc-Steffen Kaesz <mskaesz@protonmail.com>
2022-04-23 20:59:06 +0200

    refactor

--- 05_configure_vault_tfc_secrets/main.tf
+++ 05_configure_vault_tfc_secrets/main.tf
@@ -6,9 +6,3 @@ resource "vault_terraform_cloud_secret_backend" "terraform" {
   description = "Manages the Terraform Cloud backend"
   token       = var.tfc_user_token 
 }
-
-resource "vault_terraform_cloud_secret_role" "terraform" {
-  backend      = vault_terraform_cloud_secret_backend.terraform.backend
-  name         = "secured-pipeline-project1"
-  team_id      = file("../03_setup_tfc/temp_data/tfc_secured-pipeline-project1_id")
-}
