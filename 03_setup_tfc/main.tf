704b5bf32b6e61a76635ab4d5e036f532182f27a
Marc-Steffen Kaesz <mskaesz@protonmail.com>
2022-04-23 20:53:05 +0200

    refactor

--- 03_setup_tfc/main.tf
+++ 03_setup_tfc/main.tf
@@ -7,32 +7,6 @@ data "tfe_organization" "mkaesz-dev" {
   name = "mkaesz-dev"
 }
 
-resource "tfe_team" "secured-pipeline-project1" {
-  name         = "secured-pipeline-project1"
-  organization = data.tfe_organization.mkaesz-dev.name
-  visibility   = "secret"
-  organization_access {
-    manage_vcs_settings = false
-    manage_policies     = false
-    manage_workspaces   = false
-  }
-}
-
-resource "tfe_team" "team2" {
-  name         = "team2"
-  organization = data.tfe_organization.mkaesz-dev.name
-}
-
-resource "local_file" "secured-pipeline-project1_id" {
-    content  = tfe_team.secured-pipeline-project1.id
-    filename = "temp_data/tfc_secured-pipeline-project1_id"
-}
-
-resource "local_file" "tfc_team2_id" {
-    content  = tfe_team.team2.id
-    filename = "temp_data/tfc_team2_id"
-}
-
 resource "local_file" "org" {
     content  = data.tfe_organization.mkaesz-dev.name
     filename = "temp_data/tfc_org_name"
--- 07_example_project/tfc.tf
+++ 07_example_project/tfc.tf
@@ -1,6 +1,17 @@
 provider tfe {
 }
 
+resource "tfe_team" "secured-pipeline-project1" {
+  name         = "secured-pipeline-project1"
+  organization = file("../03_setup_tfc/temp_data/tfc_org_name")
+  visibility   = "secret"
+  organization_access {
+    manage_vcs_settings = false
+    manage_policies     = false
+    manage_workspaces   = false
+  }
+}
+
 resource "tfe_workspace" "secured-pipeline-project1" {
   name         = "secured-pipeline-project1"
   organization = file("../03_setup_tfc/temp_data/tfc_org_name")
@@ -9,6 +20,6 @@ resource "tfe_workspace" "secured-pipeline-project1" {
 
 resource "tfe_team_access" "secured-pipeline-project1" {
   access       = "write"
-  team_id      = file("../03_setup_tfc/temp_data/tfc_secured-pipeline-project1_id")
+  team_id      = tfe_team.secured-pipeline-project1.id
   workspace_id = tfe_workspace.secured-pipeline-project1.id
 }
