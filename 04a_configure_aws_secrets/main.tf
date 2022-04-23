e1d0d3171dd710b22e4b78dcd4c20f445b85c374
Marc-Steffen Kaesz <mskaesz@protonmail.com>
2022-04-23 15:41:37 +0200

    cleanup

--- 04a_configure_aws_secrets/main.tf
+++ 04a_configure_aws_secrets/main.tf
@@ -3,7 +3,7 @@ provider "vault" {}
 resource "vault_aws_secret_backend" "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
-  path       = "${var.name}"
+  path       = "aws"
   description = "The AWS secrets backend used to generate dynamic credentials."
   region = "eu-central-1"
 
--- 04a_configure_aws_secrets/output.tf
+++
@@ -1,7 +0,0 @@
-output "backend" {
-  value = vault_aws_secret_backend.aws.path
-}
-
-output "role" {
-  value = vault_aws_secret_backend_role.aws-role.name
-}
