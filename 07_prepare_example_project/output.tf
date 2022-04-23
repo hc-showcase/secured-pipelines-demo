bef20f8844b1a439387887c5b90cc6c070455a48
Marc-Steffen Kaesz <mskaesz@protonmail.com>
2022-04-23 15:53:13 +0200

    cleanup

--- 07_prepare_example_project/output.tf
+++
@@ -1,4 +0,0 @@
-output "project-url" {
-    description = "URL of the Gitlab project"
-    value       = gitlab_project.secured-pipeline-project1.web_url
-}
--- 07_prepare_example_project/tfc.tf
+++
@@ -1,7 +0,0 @@
-provider tfe {
-}
-
-resource "tfe_workspace" "secured-pipeline-project1" {
-  name         = "secured-pipeline-project1"
-  organization = file("../03_setup_tfc/temp_data/tfc_org_name")
-}
